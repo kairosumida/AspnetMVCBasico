71a5a952d7220c01c5fd1be427537e4e32aedd50
Kairo Sumida <kairosumida@hotmail.com>
2018-01-22 13:50:49 -0200

    Adicionado o controller Operacao

---
+++ Aula03/Controllers/OperacaoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula03.Controllers
+{
+    public class OperacaoController : Controller
+    {
+        // GET: Operacao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
