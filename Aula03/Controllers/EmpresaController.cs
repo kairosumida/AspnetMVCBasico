3ed369e275d66edb583b1e6307d28ef94685c30d
Kairo Sumida <kairosumida@hotmail.com>
2018-01-22 12:50:03 -0200

    Criado a controller Empresa na Aula03

---
+++ Aula03/Controllers/EmpresaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula03.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        // GET: Empresa
+        public string Index()
+        {
+            string texto = "<b>Wincomp Informática Ltda</b><br>";
+            texto += "<i>Curso ASP NET MVC</i>";
+            return texto;
+            
+        }
+    }
+}
