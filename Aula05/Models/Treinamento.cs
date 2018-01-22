4acab64f2d20724271e64edcf914f31c96864204
Kairo Sumida <kairosumida@hotmail.com>
2018-01-22 16:09:16 -0200

    Adicionado DisplayName Aos campos de treinamento

--- Aula05/Models/Treinamento.cs
+++ Aula05/Models/Treinamento.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,9 +11,12 @@ namespace Aula05.Models
     {
         [Key]
         public int Id { get; set; }
+        [DisplayName("Treinamento")]
         public string NomeTreinamento { get; set; }
+        [DisplayName("Início")]
         public DateTime Inicio { get; set; }
         public DateTime Termino { get; set; }
+        [DisplayName("Preço")]
         public decimal Preco { get; set; }
         public decimal Desconto { get; set; }
         public string Local { get; set; }
