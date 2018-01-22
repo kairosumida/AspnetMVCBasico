e8c654e363939f17ad1b5171eaf5324c3628eb58
Kairo Sumida <kairosumida@hotmail.com>
2018-01-22 15:24:49 -0200

    Lista Produtos na View

--- Aula05/Controllers/ProdutosController.cs
+++ Aula05/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aula05.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,11 @@ namespace Aula05.Controllers
 {
     public class ProdutosController : Controller
     {
+        private Aula05Context db = new Aula05Context();
         // GET: Produtos
         public ActionResult Index()
         {
-            return View();
+            return View(db.Produtos.ToList());
         }
     }
 }
