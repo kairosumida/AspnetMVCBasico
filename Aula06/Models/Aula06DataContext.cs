c241a43ecdc7f24d943a4ae7407f5957347e2193
Kairo Sumida <kairosumida@hotmail.com>
2018-01-22 21:36:34 -0200

    Criado o DBContext

---
+++ Aula06/Models/Aula06DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Aula06.Models
+{
+    public class Aula06DataContext : DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
