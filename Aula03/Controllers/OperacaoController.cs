c3825d8d5bcf9526792411d2312085106b20d784
Kairo Sumida <kairosumida@hotmail.com>
2018-01-22 14:02:09 -0200

    Adicionado método subtração em OperacaoController

--- Aula03/Controllers/OperacaoController.cs
+++ Aula03/Controllers/OperacaoController.cs
@@ -19,6 +19,11 @@ namespace Aula03.Controllers
             texto += "<p>Resultado: " + (op1 + op2) + "</p>";
             return texto;
         }
-
+        public string Subtracao(double op1, double op2)
+        {
+            string texto = "<h1>Operação Subtracao</h1>";
+            texto += "<p>Resultado: " + (op1 - op2) + "</p>";
+            return texto;
+        }
     }
 }
