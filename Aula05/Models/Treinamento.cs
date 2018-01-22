e5bcd3b967b422ba292560d8e18a54dfa098625f
Kairo Sumida <kairosumida@hotmail.com>
2018-01-22 15:58:20 -0200

    Criado TreinamentoModel e seus campos

---
+++ Aula05/Models/Treinamento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Aula05.Models
+{
+    public class Treinamento
+    {
+        [Key]
+        public int Id { get; set; }
+        public string NomeTreinamento { get; set; }
+        public DateTime Inicio { get; set; }
+        public DateTime Termino { get; set; }
+        public decimal Preco { get; set; }
+        public decimal Desconto { get; set; }
+        public string Local { get; set; }
+    }
+}
