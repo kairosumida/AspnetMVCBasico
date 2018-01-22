71a35f8b55073d4990143e12455ad74bf713e6c5
Kairo Sumida <kairosumida@hotmail.com>
2018-01-22 16:03:44 -0200

    Criado Scaffold de Treinamento

---
+++ Aula05/Controllers/TreinamentosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Aula05.Models;
+
+namespace Aula05.Controllers
+{
+    public class TreinamentosController : Controller
+    {
+        private Aula05Context db = new Aula05Context();
+
+        // GET: Treinamentos
+        public ActionResult Index()
+        {
+            return View(db.Treinamentos.ToList());
+        }
+        
+    }
+}
