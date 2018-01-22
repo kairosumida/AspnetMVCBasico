b23dc3e0e3702bfc2df913462930a8888f802a23
Kairo Sumida <kairosumida@hotmail.com>
2018-01-22 14:52:23 -0200

    Adicionado produtoModel

---
+++ Aula05/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aula05.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
