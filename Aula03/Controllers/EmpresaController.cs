1db5be6e9eaeb5c6fa24ac5144cba9dfac20686f
Kairo Sumida <kairosumida@hotmail.com>
2018-01-22 13:50:07 -0200

    Adicionado os métodos da EmpresaController

--- Aula03/Controllers/EmpresaController.cs
+++ Aula03/Controllers/EmpresaController.cs
@@ -14,7 +14,22 @@ namespace Aula03.Controllers
             string texto = "<b>Wincomp Informática Ltda</b><br>";
             texto += "<i>Curso ASP NET MVC</i>";
             return texto;
+        }
+        public string Endereco()
+        {
+            string texto = "<b>Wincomp Informática Ltda</b><br>";
+            texto += "<i>www.wincomp.com.br</i>";
+            return texto;
+        }
+
+
+        //Um exemplo para utilizar o metedo curso
+        //No endereço digitar = http://localhost:11111/Empresa/Curso?nome=Kairo&curso=ASP NET MVC 
+        public string Curso(string nome, string curso)
+        {
             
+            string texto = "O aluno " + nome + " esta matriculado no curso " + curso;
+            return HttpUtility.HtmlEncode(texto);
         }
     }
 }
