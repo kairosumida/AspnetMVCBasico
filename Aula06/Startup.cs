1c438a108a606e8df63c15fd25b0743756934cf7
Kairo Sumida <kairosumida@hotmail.com>
2018-01-22 19:39:13 -0200

    Adicionado projeto aula06

---
+++ Aula06/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Aula06.Startup))]
+namespace Aula06
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
