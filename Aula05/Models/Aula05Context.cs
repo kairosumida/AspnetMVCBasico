323fa28c3f4edfdb6e729031ab115b0c6674699c
Kairo Sumida <kairosumida@hotmail.com>
2018-01-22 14:54:00 -0200

    Criado DbContext com Produtos

---
+++ Aula05/Models/Aula05Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Aula05.Models
+{
+    public class Aula05Context : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+    }
+}
