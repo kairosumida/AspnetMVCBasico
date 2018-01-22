0d2bf1e061b91c58b8c2792300532ca3f80ea8b9
Kairo Sumida <kairosumida@hotmail.com>
2018-01-22 14:49:56 -0200

    Criado o projeto da aula05

---
+++ Aula05/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Aula05.MvcApplication" Language="C#" %>
---
+++ Aula05/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Aula05.Startup))]
+namespace Aula05
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
