7bb0177ac7b4cb347a1867220fb19940a998bcff
Kairo Sumida <kairosumida@hotmail.com>
2018-01-23 01:02:03 -0200

    Criado Projeto Aula07

---
+++ Aula07/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Aula07.Startup))]
+namespace Aula07
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
