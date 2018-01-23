3ca6daed4f295c0870d0c09ec6199357ec59863a
Kairo Sumida <kairosumida@hotmail.com>
2018-01-23 03:04:00 -0200

    Finalizado o projeto

---
+++ Aula08/Models/Aula08DBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Aula08.Models
+{
+    public class Aula08DBContext :DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
---
+++ Aula08/Models/Veiculo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Aula08.Models
+{
+    public enum Marca
+    {
+        VW, Ford, Honda
+    }
+    public enum Combustivel
+    {
+        Flex, Etanol, Gasolina, Diesel, Gas
+    }
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        public Marca Marca { get; set; }
+        public string Modelo { get; set; }
+        [DisplayName("Observação")]
+        public string Observacao { get; set; }
+        [DisplayName("Preço")]
+        public decimal Preco { get; set; }
+        public string Estado { get; set; }
+        public Combustivel Combustivel { get; set; }
+        public bool Publicar { get; set; }
+    }
+}
