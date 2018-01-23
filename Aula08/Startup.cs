7469bae94db775876a865a040c2a67746c055f7a
Kairo Sumida <kairosumida@hotmail.com>
2018-01-23 02:33:31 -0200

    Criado o Projeto Aula08

---
+++ Aula08/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Aula08.Startup))]
+namespace Aula08
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
