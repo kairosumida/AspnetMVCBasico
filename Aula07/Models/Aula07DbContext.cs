0a7a0cf7b0c1a34bcb5175f42d0582b635231099
Kairo Sumida <kairosumida@hotmail.com>
2018-01-23 02:27:20 -0200

    Criado Veiculos na tabela

---
+++ Aula07/Models/Aula07DbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Aula07.Models
+{
+    public class Aula07DbContext : DbContext
+    {
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
---
+++ Aula07/Models/Veiculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aula07.Models
+{
+    public enum Marca
+    {
+        VW, Ford, Honda
+    }
+    public enum Combustivel
+    {
+        Flex, Etanol, Gasolina, Diesel, Gas
+    }
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public Marca Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Observacao { get; set; }
+        public decimal Preco { get; set; }
+        public string Estado { get; set; }
+        public Combustivel Combustivel { get; set; }
+        public bool Publicar { get; set; }
+    }
+}
