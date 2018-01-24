f3a7b6d41854c4aa5cc64cdc2c9355e8ad6c29c7
Kairo Sumida <kairosumida@hotmail.com>
2018-01-24 21:17:43 -0200

    Criado Model Usuario

---
+++ Aula10/Models/Aula10DbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aula10.Models
+{
+    public class Aula10DbContext : DbContext
+    {
+    }
+}
---
+++ Aula10/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aula10.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CEP { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Senha { get; set; }
+        public string SenhaRepetir { get; set; }
+    }
+}
