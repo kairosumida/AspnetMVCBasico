b435354274f9b83d3cf5495ff8fc2e99a6a2eb02
Kairo Sumida <kairosumida@hotmail.com>
2018-01-24 21:19:25 -0200

    Criada Conexao com o BD

--- Aula10/Models/Aula10DbContext.cs
+++ Aula10/Models/Aula10DbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,6 @@ namespace Aula10.Models
 {
     public class Aula10DbContext : DbContext
     {
+        public DbSet<Usuario> Usuarios { get; set; }
     }
 }
