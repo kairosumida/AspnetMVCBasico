a350ca0a59a523502db0160da33af472c0c1c813
Kairo Sumida <kairosumida@hotmail.com>
2018-01-24 22:01:42 -0200

    Criado o model de Treinamento

---
+++ Aula10/Models/Treinamento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Aula10.Models
+{
+    public class Treinamento
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Treinamento")]
+        [Required(ErrorMessage="Favor colocar o nome do treinamento!")]
+        [StringLength(30,ErrorMessage ="O treinamento deve ter de 2 a 30 caracteres",MinimumLength =2)]
+        public string NomeTreinamento { get; set; }
+        [DisplayName("Início")]
+        public DateTime Inicio { get; set; }
+        [DisplayName("Término")]
+        public DateTime Termino { get; set; }
+        [DisplayName("Preço")]
+        [Range(typeof(decimal), "10", "1000", ErrorMessage ="O preço deve de 10 a 1000")]
+        public decimal Preco { get; set; }
+        [Range(typeof(decimal), "0", "100", ErrorMessage = "O desconto deve ser de 0 a 100")]
+        public decimal Desconto { get; set; }
+        [Required(ErrorMessage ="Local deve ser preenchido")]
+        [RegularExpression(@"[ABC]", ErrorMessage = "Digite apenas A, B ou C")]
+        public string Local { get; set; }
+    }
+}
--- Aula10/Models/Usuario.cs
+++ Aula10/Models/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,21 @@ namespace Aula10.Models
     public class Usuario
     {
         public int Id { get; set; }
+        [Required(ErrorMessage="Favor preencher o nome!")]
+        [StringLength(20, ErrorMessage ="O nome deve ter de 5 a 20 caracteres", MinimumLength =5)]
         public string Nome { get; set; }
+        [Required(ErrorMessage ="Favor preemcjer p CEP!")]
+        [RegularExpression(@"\d{5}-\d{3}|\d{8}", ErrorMessage ="Utilize o formato 00000-000")]
         public string CEP { get; set; }
+        [EmailAddress(ErrorMessage = "Email incorreto")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "O campo Nascimento é de preenchimento obrigatório")]
         public DateTime DataNascimento { get; set; }
+        [DataType(DataType.Password)]
         public string Senha { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar a senha")]
+        [Compare("Senha", ErrorMessage = "A senha e a confirmação da senha são diferentes")]
         public string SenhaRepetir { get; set; }
     }
 }
