dc17d31b0f2cb11dc05ba9c01463c9a72040f28b
Kairo Sumida <kairosumida@hotmail.com>
2018-01-24 22:26:54 -0200

    Alterado documentos para atender melhor as especificações

--- Aula10/Controllers/UsuariosController.cs
+++ Aula10/Controllers/UsuariosController.cs
@@ -13,37 +13,17 @@ namespace Aula10.Controllers
     public class UsuariosController : Controller
     {
         private Aula10DbContext db = new Aula10DbContext();
-
-        // GET: Usuarios
+        
         public ActionResult Index()
         {
             return View(db.Usuarios.ToList());
         }
-
-        // GET: Usuarios/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Usuario usuario = db.Usuarios.Find(id);
-            if (usuario == null)
-            {
-                return HttpNotFound();
-            }
-            return View(usuario);
-        }
-
-        // GET: Usuarios/Create
+       
         public ActionResult Create()
         {
             return View();
         }
-
-        // POST: Usuarios/Create
-        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
-        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,Nome,CEP,Email,DataNascimento,Senha,SenhaRepetir")] Usuario usuario)
@@ -57,64 +37,7 @@ namespace Aula10.Controllers
 
             return View(usuario);
         }
-
-        // GET: Usuarios/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Usuario usuario = db.Usuarios.Find(id);
-            if (usuario == null)
-            {
-                return HttpNotFound();
-            }
-            return View(usuario);
-        }
-
-        // POST: Usuarios/Edit/5
-        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
-        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Nome,CEP,Email,DataNascimento,Senha,SenhaRepetir")] Usuario usuario)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(usuario).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(usuario);
-        }
-
-        // GET: Usuarios/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Usuario usuario = db.Usuarios.Find(id);
-            if (usuario == null)
-            {
-                return HttpNotFound();
-            }
-            return View(usuario);
-        }
-
-        // POST: Usuarios/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Usuario usuario = db.Usuarios.Find(id);
-            db.Usuarios.Remove(usuario);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing)
--- Aula10/Models/Aula10DbContext.cs
+++ Aula10/Models/Aula10DbContext.cs
@@ -9,5 +9,7 @@ namespace Aula10.Models
     public class Aula10DbContext : DbContext
     {
         public DbSet<Usuario> Usuarios { get; set; }
+
+        public System.Data.Entity.DbSet<Aula10.Models.Treinamento> Treinamentoes { get; set; }
     }
 }
