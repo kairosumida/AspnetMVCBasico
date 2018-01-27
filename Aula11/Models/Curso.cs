9d803c7b9ce14c29c03867810e641fa8d72df334
Jairo Sumida <kairo.sumida@fatec.sp.gov.br>
2018-01-27 12:17:18 -0200

    Criado metade do projeto, preparando para o migration

---
+++ Aula11/Models/Curso.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Aula11.Models
+{
+    public class Curso
+    {
+        [Key]
+        public int CursoID { get; set; }
+        public string Nome { get; set; }
+        [Range(typeof(decimal), "10", "1000", ErrorMessage = "O preço deve de 1 a 100000")]
+        [DisplayName("Preço")]
+        public decimal Preco { get; set; }
+    }
+}
